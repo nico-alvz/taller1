54ac912dd6ce8a03d75104e7f3a6d4144d65358d
Nicolás Alvarez <nicolas.alvarez02@alumnos.ucn.cl>
2025-04-22 18:08:14 -0400

    fix: server.js on lo

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || "127.0.0.1";
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
     console.log(`🚀 Server running at http://${HOST}:${PORT}`);
